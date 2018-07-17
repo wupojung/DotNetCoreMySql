7863284ad1efcdfffa8c445e70397132befe9d46
Po-Jung Wu <wupojung@gmail.com>
2018-07-18 00:43:46 +0800

    加入專案檔案。

---
+++ DotNetCoreMySql.Repository/MovieContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DotNetCoreMySql.Repository
+{
+    public class MovieContextFactory : IDesignTimeDbContextFactory<MovieContext>
+    {
+        public MovieContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MovieContext>();
+            optionsBuilder.UseMySQL("server=localhost;database=movie;user=movie;password=movie;SslMode=none");
+            return new MovieContext(optionsBuilder.Options);
+        }
+    }
+}
